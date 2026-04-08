8d9b97215cd90a716256de906154a12bbdde90f1
DanilDill <dldanildm@mail.ru>
2026-04-08 15:20:19 +0300

    add clang-format, clang-tidy, add header-only math functions

---
+++ include/libmath.h
@@ -0,0 +1,74 @@
+#pragma once
+#include <climits>
+
+namespace math 
+{
+    int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    int sub(int a, int b)
+    {
+        return a - b;
+    }
+
+    int mul(int a, int b)
+    {
+        return a * b;
+    }
+
+    int div(int a, int b, int& err)
+    {
+        err = 0;
+        if (b == 0 )
+        {
+            err = -1;
+            return 0;
+        }
+        return a / b;
+    }
+
+    int mod(int a, int b, int& err)
+    {
+        err = 0;
+        if (b == 0)
+        {
+            err = -1;
+            return 0;    /* code */
+        }
+        return a % b;
+    }
+
+    int pow(int a, int b)
+    {
+        int result = 1;
+        for (size_t i = 0; i < b; i++)
+        {
+            if (result > INT_MAX / a)
+            {
+                return -1;
+            }
+
+            result *= a;
+        }
+        return result; 
+        
+    }
+
+    int factorial(int a)
+    {
+        static int maxFactorial = 12;
+        int result = 1;
+        if (result == a)
+        {
+            return result;
+        }
+        if (a > maxFactorial)
+        {
+            return -1;
+        }
+        return a * factorial(a - 1);
+    }
+
+}
