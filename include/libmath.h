7832659882f9ec2f0cc6ab8414cd1d5059dacb02
DanilDill <dldanildm@mail.ru>
2026-04-09 18:09:14 +0300

    add overflow check

--- include/libmath.h
+++ include/libmath.h
@@ -1,77 +1,83 @@
 #pragma once
-#include <climits>
 #include <math.h>
+
+#include <climits>
 #define DIV_BY_ZERO -1
 #define OVERFLOW -2
-namespace math 
+namespace math
 {
-    int add(int a, int b)
-    {
-        return a + b;
-    }
+int add(int a, int b)
+{
+    return a + b;
+}
 
-    int sub(int a, int b)
-    {
-        return a - b;
-    }
+int sub(int a, int b)
+{
+    return a - b;
+}
 
-    int mul(int a, int b)
+int mul(int a, int b,int& err)
+{
+    int result = a*b;
+    if(abs(result) > INT_MAX / abs(a))
     {
-        return a * b;
+        err = OVERFLOW;
     }
+    return result;
+}
 
-    int div(int a, int b, int& err)
+int div(int a, int b, int& err)
+{
+    err = 0;
+    if (b == 0)
     {
-        err = 0;
-        if (b == 0 )
+        err = DIV_BY_ZERO;
+        return 0;
+    }
+    return a / b;
+}
+
+int mod(int a, int b, int& err)
+{
+    err = 0;
+    if (b == 0)
+    {
+        err = -1;
+        return 0; /* code */
+    }
+    return a % b;
+}
+
+int pow(int a, unsigned int b, int& err)
+{
+    int result = 1;
+    for (unsigned int i = 0; i < b; i++)
+    {
+        if (abs(result) > INT_MAX / abs(a))
         {
-            err = DIV_BY_ZERO;
-            return 0;
-        }
-        return a / b;
-    }
-
-    int mod(int a, int b, int& err)
-    {
-        err = 0;
-        if (b == 0)
-        {
-            err = -1;
-            return 0;    /* code */
-        }
-        return a % b;
-    }
-
-    int pow(int a, unsigned int b, int& err)
-    {
-        int result = 1;
-        for (unsigned int i = 0; i < b; i++)
-        {
-            if (abs(result) > INT_MAX / a)
-            {
-                err =  OVERFLOW;
-                return result; 
-            }
-
-            result *= a;
-        }
-        return result; 
-        
-    }
-
-    int factorial(unsigned int a)
-    {
-        static int maxFactorial = 12;
-        int result = 1;
-        if (result == a)
-        {
+            err = OVERFLOW;
             return result;
         }
-        if (a > maxFactorial)
-        {
-            return OVERFLOW;
-        }
-        return a * factorial(a - 1);
-    }
 
+        result *= a;
+    }
+    return result;
 }
+
+int factorial(unsigned int a, int& err)
+{
+    static int maxFactorial = 12;
+    int result = 1;
+    if (result == a)
+    {
+        return result;
+    }
+    if (a > maxFactorial)
+    {
+        err = OVERFLOW;
+        return a;
+    }
+    return a * factorial(a - 1, err);
+}
+
+} // namespace math
